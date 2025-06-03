e0a408ee44b85eb05e6c301bb77f4ece4dd4f7b7
KN0WLES <202401604@est.umss.edu>
2025-06-03 06:18:47 -0400

    add: manejo de errores relacionados con los horarios

---
+++ src/main/java/unicorn/exceptions/ScheduleException.java
@@ -0,0 +1,23 @@
+package unicorn.exceptions;
+
+public class ScheduleException extends Exception {
+    public ScheduleException(String message) {
+        super(message);
+    }
+
+    public static ScheduleException notFound() {
+        return new ScheduleException("Horario no encontrado");
+    }
+
+    public static ScheduleException timeConflict() {
+        return new ScheduleException("Conflicto de horario detectado");
+    }
+
+    public static ScheduleException invalidTimeFormat() {
+        return new ScheduleException("Formato de hora inválido (use HH:mm)");
+    }
+
+    public static ScheduleException invalidDay() {
+        return new ScheduleException("Día de la semana inválido");
+    }
+}
